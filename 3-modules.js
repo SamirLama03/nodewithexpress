0309fe2e3e0090a14d1ef981af6002bc52b637d8
SamirLama03 <117245671+SamirLama03@users.noreply.github.com>
2023-08-11 13:20:59 +0545

    part 7

---
+++ 3-modules.js
@@ -0,0 +1,10 @@
+// Modules
+const names = require("./4-firstmodules");
+const sayHi = require("./5-utils");
+const data = require("./6-alternative");
+require("./7-mindGrenade");
+// console.log(data);
+
+// sayHi("sam");
+// sayHi(names.samir);
+// sayHi(names.sonam);
