166a05b0d34e051e16e62316dab1318c5617e11a
SamirLama03 <117245671+SamirLama03@users.noreply.github.com>
2023-09-13 20:47:36 +0545

    Express Api

--- Express-tut/app.js
+++ Express-tut/app.js
@@ -1,21 +1,30 @@
-const http = require("http");
+const express = require("express");
+const app = express();
+const { products } = require("./data");
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-
-  if (url === "/") {
-    res.writeHead(200, { "content-type": "text/html " });
-    res.write("<h1>home page <h1>");
-    res.end();
-  } else if (url === "/about") {
-    res.writeHead(200, { "content-type": "text/html " });
-    res.write("<h1>About page <h1>");
-    res.end();
-  } else {
-    res.writeHead(404, { "content-type": "text/html " });
-    res.write("<h1>Page not found <h1>");
-    res.end();
-  }
+app.get("/", (req, res) => {
+  res.send('<h1>Homepage</h1><a href="api/products" >product</a>');
 });
 
-server.listen(5000);
+app.get("/api/products", (req, res) => {
+  const newProducts = products.map((product) => {
+    const { id, name, image } = product;
+    return { id, name, image };
+  });
+  res.json(newProducts);
+});
+app.get("/api/products/:productID", (req, res) => {
+  const { productID } = req.params;
+  const singleProduct = products.find(
+    (product) => product.id === Number(productID)
+  );
+  if (!singleProduct) {
+    return res.status(404).send("Product does not exist");
+  }
+
+  res.json(singleProduct);
+});
+
+app.listen(5000, () => {
+  console.log("Server is listening on port 5000.....");
+});
