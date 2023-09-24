5c527df373b8d65e9744aaa60e68f763649c2434
SamirLama03 <117245671+SamirLama03@users.noreply.github.com>
2023-09-24 17:19:01 +0545

    method and middleware

--- Express-tut/app.js
+++ Express-tut/app.js
@@ -1,30 +1,66 @@
 const express = require("express");
 const app = express();
-const { products } = require("./data");
+let { people } = require("./data");
 
-app.get("/", (req, res) => {
-  res.send('<h1>Homepage</h1><a href="api/products" >product</a>');
+app.use(express.static("./methods-public"));
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/api/people", (req, res) => {
+  res.status(200).json({ success: true, data: people });
 });
 
-app.get("/api/products", (req, res) => {
-  const newProducts = products.map((product) => {
-    const { id, name, image } = product;
-    return { id, name, image };
-  });
-  res.json(newProducts);
+app.get("/api/people/:id", (req, res) => {
+  const { id } = req.params;
+  res.status(200).json({ success: true, id });
 });
-app.get("/api/products/:productID", (req, res) => {
-  const { productID } = req.params;
-  const singleProduct = products.find(
-    (product) => product.id === Number(productID)
-  );
-  if (!singleProduct) {
-    return res.status(404).send("Product does not exist");
+
+app.post("/api/people", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    res.status(400).json({ success: false, msg: "provide a name" });
+  } else {
+    res.status(201).json({ success: true, person: name });
   }
+});
 
-  res.json(singleProduct);
+app.post("/api/postman/people", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "please provide valid credentials" });
+  } else {
+    res.status(201).json({ success: true, data: [...people, name] });
+  }
+});
+
+app.post("/login", (req, res) => {
+  const { name } = req.body;
+  if (name) {
+    res.status(201).send(`Welcome${name}`);
+  } else {
+    res.status(401).send("Please provide credentials");
+  }
+});
+
+app.put("api/people/:id", (req, res) => {
+  const { name } = req.body;
+  const { id } = req.params;
+
+  const person = people.find((person) => person.id === Number(id));
+
+  if (!person) {
+    res.status(404).json({ success: false, msg: ` no person with ${id}` });
+  }
+  const newPerson = people.map((person) => {
+    if (person.id === Number(id)) {
+      person.name = name;
+    }
+    return person;
+  });
+  res.status(200).json({ success: true, data: newPerson });
 });
 
 app.listen(5000, () => {
-  console.log("Server is listening on port 5000.....");
+  console.log("server is listening on port 5000");
 });
--- Express-tut/final/09-middleware-use.js
+++ Express-tut/final/09-middleware-use.js
@@ -1,24 +1,51 @@
-const express = require('express')
-const app = express()
-const logger = require('./logger')
-const authorize = require('./authorize')
+const express = require("express");
+const app = express();
+const logger = require("./logger");
+const authorize = require("./authorize");
 //  req => middleware => res
-app.use([logger, authorize])
+app.use([logger, authorize]);
 // api/home/about/products
-app.get('/', (req, res) => {
-  res.send('Home')
-})
-app.get('/about', (req, res) => {
-  res.send('About')
-})
-app.get('/api/products', (req, res) => {
-  res.send('Products')
-})
-app.get('/api/items', (req, res) => {
-  console.log(req.user)
-  res.send('Items')
-})
+app.get("/", (req, res) => {
+  res.send("Home");
+});
+app.get("/about", (req, res) => {
+  res.send("About");
+});
+app.get("/api/products", (req, res) => {
+  res.send("Products");
+});
+app.get("/api/items", (req, res) => {
+  console.log(req.user);
+  res.send("Items");
+});
 
 app.listen(5000, () => {
-  console.log('Server is listening on port 5000....')
-})
+  console.log("Server is listening on port 5000....");
+});
+
+// const express = require("express");
+// const app = express();
+
+// const authorize = (req, res, next) => {
+//   const { user } = req.query;
+//   if (user === "john") {
+//     req.user = { name: "john", id: 3 };
+//     next();
+//   } else {
+//     res.status(401).send("Unauthorized");
+//   }
+// };
+
+// app.use(authorize);
+
+// app.get("/", (req, res) => {
+//   res.send("Home");
+// });
+
+// app.get("/about", (req, res) => {
+//   res.send("About");
+// });
+
+// app.listen(5000, () => {
+//   console.log("server is listening on port 5000....");
+// });
